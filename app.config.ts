0e8997d79d3fa35085ef4ad9b06da6d96d900ed9
Ivan Kiral <kiralivan@gmail.com>
2025-08-13 19:17:00 +0200

    refactor: simplify app config   exports and structure

--- app.config.ts
+++ app.config.ts
@@ -2,21 +2,20 @@ import { defineConfig } from "@solidjs/start/config";
 import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-	vite: {
-		plugins: [
-			visualizer({
-				filename: "bundle-report.html",
-				template: "treemap",
-				gzipSize: true,
-				brotliSize: true,
-				open: true,
-			}) as unknown as import("vite").PluginOption,
-		],
-	},
-	server: {
-		preset: "github-pages",
-		prerender: {
-			routes: ["/", "/en", "/sk"],
-		},
-	},
+  vite: {
+    plugins: [
+      visualizer({
+        filename: "bundle-report.html",
+        template: "treemap",
+        gzipSize: true,
+        brotliSize: true,
+      }) as unknown as import("vite").PluginOption,
+    ],
+  },
+  server: {
+    preset: "github-pages",
+    prerender: {
+      routes: ["/", "/en", "/sk"],
+    },
+  },
 });
