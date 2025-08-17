26d822b5129d80ff1732997e85d5e1a33a1edc6d
Ivan Kiral <kiralivan@gmail.com>
2025-08-17 16:54:03 +0200

    change favicon

--- src/app.tsx
+++ src/app.tsx
@@ -12,6 +12,10 @@ export default function App() {
         <MetaProvider>
           <Title>Cookmark</Title>
           <Suspense>{props.children}</Suspense>
+          <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/site.webmanifest" />
         </MetaProvider>
       )}
     >
